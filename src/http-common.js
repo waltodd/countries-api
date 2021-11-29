713263eae76932f5e74a8db3d2f0641471a5c73a
Walt ODD <86666367+waltodd@users.noreply.github.com>
2021-11-29 20:28:23 +0000

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://restcountries.com/v3.1",
+  baseURL: "https://restcountries.com/v2",
   headers: {
     "Content-type": "application/json"
   }
